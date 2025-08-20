af753e06a54dc4161e5c24ab830ed072fccaf30a
Danbradleystakusopere <danstakus@gmail.com>
2025-08-21 01:20:58 +0300

    Add Author.articles() and Author.magazines() with Article.all tracking

--- lib/article.py
+++ lib/article.py
@@ -1,18 +1,15 @@
-
-
 from lib.author import Author
 from lib.magazine import Magazine
 
 class Article:
+    all = []  
+
     def __init__(self, author, magazine, title):
-        
         if type(title) is not str or len(title) < 5 or len(title) > 50:
             raise Exception("Title must be a string between 5 and 50 characters")
 
-        
         if not isinstance(author, Author):
             raise Exception("Author must be an instance of Author")
-
 
         if not isinstance(magazine, Magazine):
             raise Exception("Magazine must be an instance of Magazine")
@@ -21,6 +18,7 @@ class Article:
         self._author = author
         self._magazine = magazine
 
+        Article.all.append(self)  
     @property
     def title(self):
         return self._title
--- lib/author.py
+++ lib/author.py
@@ -1,13 +1,22 @@
 
 
+from lib.article import Article
+from lib.magazine import Magazine
+
 class Author:
     def __init__(self, name):
-        
         if type(name) is not str or len(name) == 0:
             raise Exception("Name must be a non-empty string")
-        self._name = name   
+        self._name = name
 
     @property
     def name(self):
-        
         return self._name
+
+    
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
