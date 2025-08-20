640e70287556dd6b0b6c4c8ec2c1eea97aff6d12
Danbradleystakusopere <danstakus@gmail.com>
2025-08-21 01:24:45 +0300

    Add Author.add_article() and Author.topic_areas() methods

--- lib/author.py
+++ lib/author.py
@@ -1,5 +1,3 @@
-
-
 from lib.article import Article
 from lib.magazine import Magazine
 
@@ -13,10 +11,20 @@ class Author:
     def name(self):
         return self._name
 
-    
     def articles(self):
         return [article for article in Article.all if article.author == self]
 
-    
     def magazines(self):
         return list({article.magazine for article in self.articles()})
+
+    
+    def add_article(self, magazine, title):
+        if not isinstance(magazine, Magazine):
+            raise Exception("Must pass a Magazine instance")
+        return Article(self, magazine, title)
+
+    
+    def topic_areas(self):
+        if len(self.articles()) == 0:
+            return None
+        return list({magazine.category for magazine in self.magazines()})
